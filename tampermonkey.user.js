7c863cd43153b2fa33796df2629b1c353a1c1632
Huu Phuoc <118438452+Pinnnndz@users.noreply.github.com>
2025-11-29 22:04:14 +0700

    Create tampermonkey.user.js

---
+++ tampermonkey.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         PinG ORION COMMANDER VPROMAX
+// @namespace    http://tampermonkey.net/
+// @version      VMAX
+// @description  VMAX: Live Stats Engine (Real-time Sync), Smart Update Notification, Safety Mode, and UI Enhancements.
+// @author       Huu Phuoc
+// @match        https://locket.binhake.dev/celebrity.html
+// @match        https://locket.binhake.dev/settings.html
+// @require      https://unpkg.com/react@18.2.0/umd/react.production.min.js
+// @require      https://unpkg.com/react-dom@18.2.0/umd/react-dom.production.min.js
+// @require      https://raw.githubusercontent.com/Pinnnndz/locket-autocelebrity-vpromax/main/auto-celeb.main.js
+// @grant        GM_addStyle
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @run-at       document-end
+// @icon         https://sf-static.upanhlaylink.com/img/image_20251124a53c851ee723c755f9e19b8cec568a9e.jpg
+// ==/UserScript==
