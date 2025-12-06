166bc22f7c5b9fb18f19bf56d2452f7fa5cb5196
Huu Phuoc <118438452+Pinnnndz@users.noreply.github.com>
2025-12-06 12:55:06 +0700

    Update UserScript metadata for ORION INFINITY

--- tampermonkey.user.js
+++ tampermonkey.user.js
@@ -1,17 +1,24 @@
 // ==UserScript==
-// @name         PinG ORION COMMANDER VPROMAX
+// @name         Auto Celebrity Interceptor - ORION INFINITY (GRID FIX)
 // @namespace    http://tampermonkey.net/
-// @version      VMAX
-// @description  VMAX: Live Stats Engine (Real-time Sync), Smart Update Notification, Safety Mode, and UI Enhancements.
-// @author       Huu Phuoc
+// @version      X.2003-GRIDFIX
+// @description  Full ETERNITY PRO UI + Smart Engine Fix + Nuke Notices
+// @author       Huu Phuoc & AI Assistant
 // @match        https://locket.binhake.dev/celebrity.html
 // @match        https://locket.binhake.dev/settings.html
 // @require      https://unpkg.com/react@18.2.0/umd/react.production.min.js
 // @require      https://unpkg.com/react-dom@18.2.0/umd/react-dom.production.min.js
 // @require      https://raw.githubusercontent.com/Pinnnndz/locket-autocelebrity-vpromax/main/auto-celeb.main.js
-// @grant        GM_addStyle
+// @connect      script.google.com
+// @connect      script.googleusercontent.com
+// @connect      api.ipify.org
+// @connect      ipv4.icanhazip.com
+// @connect      google.com
+// @connect      googleapis.com
+// @grant        GM_xmlhttpRequest
 // @grant        GM_setValue
 // @grant        GM_getValue
+// @grant        GM_addStyle
 // @run-at       document-end
 // @icon         https://sf-static.upanhlaylink.com/img/image_20251124a53c851ee723c755f9e19b8cec568a9e.jpg
 // ==/UserScript==
